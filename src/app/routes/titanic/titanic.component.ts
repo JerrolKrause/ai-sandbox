780a9a793faf3fea26817b7e7f8a3690b59eedda
Jerrol Krause <JerrolKrause@users.noreply.github.com>
2023-05-30 12:10:07 -0400

    Add UI form components

--- src/app/routes/titanic/titanic.component.ts
+++ src/app/routes/titanic/titanic.component.ts
@@ -1,7 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormBuilder } from '@angular/forms';
+import { BehaviorSubject, debounceTime, take } from 'rxjs';
+import { Memoize } from '../../shared';
 
 declare var brain: any;
+
+type Nillable<T> = {
+  [P in keyof T]?: T[P] | null | undefined;
+};
+
+interface State {
+  loading: boolean;
+  survivalOdds: number | null;
+}
 
 interface Passenger {
   PassengerId: number;
@@ -18,6 +31,17 @@ interface Passenger {
   Embarked: string;
 }
 
+interface PassengerNormalized {
+  PassengerId: number;
+  Survived?: number;
+  Pclass: number;
+  Sex: number;
+  Age: number;
+  SibSp: number;
+  Parch: number;
+  Fare: number;
+}
+
 @Component({
   selector: 'app-titanic',
   templateUrl: './titanic.component.html',
@@ -25,31 +49,112 @@ interface Passenger {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TitanicComponent implements OnInit, OnDestroy {
-  constructor(private http: HttpClient) {}
+  public state$ = new BehaviorSubject<State>({ loading: true, survivalOdds: null });
+
+  public passengerForm = this.fb.group<PassengerNormalized>({
+    PassengerId: 0,
+    Pclass: 0,
+    Sex: 0,
+    Age: 0,
+    SibSp: 0,
+    Parch: 0,
+    Fare: 0,
+  });
+
+  public sexOptions = [
+    { label: 'Male', value: 1 },
+    { label: 'Female', value: 0 },
+  ];
+
+  public siblingOptions = [
+    { label: 0, value: this.normalizeNumberRange(0, 0, 8) },
+    { label: 1, value: this.normalizeNumberRange(1, 0, 8) },
+    { label: 2, value: this.normalizeNumberRange(2, 0, 8) },
+    { label: 3, value: this.normalizeNumberRange(3, 0, 8) },
+    { label: 4, value: this.normalizeNumberRange(4, 0, 8) },
+    { label: 5, value: this.normalizeNumberRange(5, 0, 8) },
+    { label: 6, value: this.normalizeNumberRange(6, 0, 8) },
+    { label: 7, value: this.normalizeNumberRange(7, 0, 8) },
+    { label: 8, value: this.normalizeNumberRange(8, 0, 8) },
+  ];
+
+  public classOptions = [
+    { label: '1st Class', value: this.normalizeNumberRange(1, 1, 3) },
+    { label: '2nd Class', value: this.normalizeNumberRange(2, 1, 3) },
+    { label: '3rd Class', value: this.normalizeNumberRange(3, 1, 3) },
+  ];
+
+  private net = new brain.NeuralNetworkGPU();
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {
+    this.passengerForm.reset();
+    this.passengerForm.valueChanges.pipe(takeUntilDestroyed(), debounceTime(250)).subscribe(passenger => this.formChange(passenger));
+  }
 
   ngOnInit() {
     this.http.get<Passenger[]>('assets/datasets/titanic.json').subscribe(dataset => {
-      dataset.length = 20;
+      // dataset.length = 5;
       // console.log('1-0', this.normalizePassengers(dataset));
       // const passengers = dataset.map(this.normalizePassenger);
       const passengers = this.normalizePassengers(dataset);
-      console.log('Passengers', passengers);
+
       console.time('Training NN took');
       // Create a new neural network instance
-      const net = new brain.NeuralNetworkGPU();
-      net.train(passengers);
+      // const net = new brain.NeuralNetworkGPU();
+      this.net.train(passengers);
       console.timeEnd('Training NN took');
-
-      const temp = [...passengers];
-      temp.length = 20;
-      temp.forEach((p, i) => {
-        const output = net.run(p.input);
-        console.log(dataset[i].Name, p, output);
-      });
+      this.stateChange({ loading: false });
     });
   }
 
-  normalizePassengers(passengers: Passenger[]) {
+  /**
+   * Change route state
+   * @param state
+   */
+  private stateChange(state: Partial<State>) {
+    this.state$.pipe(take(1)).subscribe(stateOld => this.state$.next({ ...stateOld, ...state }));
+  }
+
+  private formChange(passenger: Nillable<PassengerNormalized>) {
+    const result = this.net.run(passenger);
+    this.stateChange({ survivalOdds: Math.floor(result.Survived * 100) });
+  }
+
+  /**
+   * Normalizes a number to a range between 0 and 1.
+   *
+   * @param {number} value - The number to be normalized.
+   * @param {number} min - The minimum value in the range of the input.
+   * @param {number} max - The maximum value in the range of the input.
+   * @returns {number} - The normalized number, now within a range of 0 to 1.
+   *
+   * @private
+   */
+  private normalizeNumberRange(value: number, min: number, max: number): number {
+    return (value - min) / (max - min);
+  }
+
+  /**
+   * Denormalizes a number from a range of 0 to 1 back to its original range.
+   *
+   * @param {number} value - The number to be denormalized (expected to be in the range [0,1]).
+   * @param {number} min - The minimum value of the original range.
+   * @param {number} max - The maximum value of the original range.
+   * @returns {number} - The denormalized number, now within the range defined by `min` and `max`.
+   *
+   * @private
+   */
+  private denormalizeNumberRange(value: number, min: number, max: number): number {
+    return value * (max - min) + min;
+  }
+
+  /**
+   *
+   * @param passengers
+   * @returns
+   */
+  @Memoize()
+  private determineValidRanges(passengers: Passenger[]) {
     // First, we calculate the range of each field
     const ranges = passengers.reduce(
       (acc, passenger) => {
@@ -73,7 +178,16 @@ export class TitanicComponent implements OnInit, OnDestroy {
         Fare: [Infinity, -Infinity],
       },
     );
+    return ranges;
+  }
 
+  private normalizePassenger(passenger: Passenger) {
+    // const ranges = this.determineValidRanges(passengers);
+    return;
+  }
+
+  normalizePassengers(passengers: Passenger[]) {
+    const ranges = this.determineValidRanges(passengers);
     // Then, we normalize each passenger
     return passengers.map(passenger => {
       return {
--- src/app/routes/titanic/titanic.module.ts
+++ src/app/routes/titanic/titanic.module.ts
@@ -1,11 +1,15 @@
 import { MasterPageModule } from '$components';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SliderModule } from 'primeng/slider';
 import { TitanicRouting } from './titanic-routing.module';
 import { TitanicComponent } from './titanic.component';
 
 @NgModule({
-  imports: [CommonModule, MasterPageModule, TitanicRouting],
+  imports: [CommonModule, MasterPageModule, SliderModule, TitanicRouting, ReactiveFormsModule, SelectButtonModule, ProgressSpinnerModule],
   declarations: [TitanicComponent],
 })
 export class TitanicModule {}
